491f8d0d8fcf5b1a8f1291fa8d1a0a00cb317706
githubdmn <dmninbox@gmail.com>
2025-04-27 18:15:19 +0200

    refactorerd data

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -4,7 +4,6 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 
-		// Run.runFirst(args);
-		Run.runSecond(args);
+		Run.runTask(args);
 	}
 }
--- src/main/java/com/example/app/Run.java
+++ src/main/java/com/example/app/Run.java
@@ -5,22 +5,39 @@ import main.java.com.example.task01.Task01;
 import main.java.com.example.utils.math.Add;
 
 public class Run {
+
+	public static final String FILE_PATH = "data/periods/period_";
+	public static final String FILE_EXTENSION = ".txt";
+
+	public static String fileName(String fileName) {
+		return new StringBuilder()
+				.append(FILE_PATH)
+				.append(fileName)
+				.append(FILE_EXTENSION)
+				.toString();
+	}
+
 	public static void run() {
 		System.out.println("Hello World from Run!");
 		Message.message();
-		System.out.println("1 + 2 = " + Add.add(1, 2));
+		System.out.println("1 + 2 = " + Add.add(1, 2) + "/n");
 	}
 
-	public static void runFirst(String[] args) {
-		Task01.runFirstImplementation("period_01.txt", "period_02.txt");
-		Task01.runFirstImplementation("period_03.txt", "period_04.txt");
-		Task01.runFirstImplementation("period_05.txt", "period_06.txt");
-		Task01.runFirstImplementation("period_07.txt", "period_08.txt");
-		Task01.runFirstImplementation("period_09.txt", "period_10.txt");
+	public static void runTask(String[] args) {
+		if (args[0].equals("1")) { runFirst(); }
+		if (args[0].equals("2")) { runSecond(); }
 	}
 
-	public static void runSecond(String[] args) {
-		Task01.runSecondImplementation("period_01.txt", "period_02.txt");
+	public static void runFirst() {
+		Task01.runFirstImplementation(fileName("01"), fileName("02"));
+		Task01.runFirstImplementation(fileName("03"), fileName("04"));
+		Task01.runFirstImplementation(fileName("05"), fileName("06"));
+		Task01.runFirstImplementation(fileName("07"), fileName("08"));
+		Task01.runFirstImplementation(fileName("09"), fileName("10"));
+	}
+
+	public static void runSecond() {
+		Task01.runSecondImplementation(fileName("01"), fileName("02"));
 	}
 
 }
